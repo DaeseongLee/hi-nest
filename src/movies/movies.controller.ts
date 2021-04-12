91130706c895dd0041a8d2537ddd9210b43701a3
DaeseongLee <47413926+DaeseongLee@users.noreply.github.com>
2021-04-12 19:58:00 +0900

    Add service and entity

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,31 +1,28 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query } from '@nestjs/common';
+import { MoviesService } from './movies.service';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { Movie } from './entities/movie.entity';
 
 @Controller('')
 export class MoviesController {
+    constructor(private readonly movieService: MoviesService) { }
     @Get()
-    getMovies() {
-        return 'This will return all movies';
+    getAll(): Movie[] {
+        return this.movieService.getAll();
     }
-
-    @Get("/search")
-    search(@Query("year") searchingYear: string) {
-        return `We are searching for a movie made after: ${searchingYear}`;
-    }
-
 
     @Get("/:id")
-    getMovieOne(@Param("id") id: String) {
-        return `This will return ${id} moivies`;
+    getOne(@Param("id") id: string) {
+        return this.movieService.getOne(id);
     }
 
     @Post()
     create(@Body() movieData) {
-        return movieData;
+        return this.movieService.create(movieData);
     }
 
     @Delete("/:id")
     remove(@Param('id') id: string) {
-        return `This will delete a movie: ${id}`;
+        return this.movieService.deleteOne(id);
     }
 
     @Patch(':id')
